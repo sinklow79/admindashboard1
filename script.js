2a9925c43cb0fca0474300857b808c6beae27d97
TotalDomination-byte <namsrai.batkhaan@gmail.com>
2022-03-30 11:02:38 +0800

    day 3.1

--- script.js
+++ script.js
@@ -15,24 +15,3 @@ function mediaQuery1200 (x) {
 const mQ1200 = window.matchMedia("(min-width: 1200px)");
 mediaQuery1200(mQ1200);
 mQ1200.addEventListener("change" ,mediaQuery1200);
-
-const navThemeToggles = document.querySelectorAll(".thm-tggl-sb-cntnr");
-for (let i = 0; i < navThemeToggles.length; i++) {
-    let elem = navThemeToggles[i];
-    elem.addEventListener('click', () => {
-        elem.children[0].classList.toggle("left-50");
-        elem.children[1].classList.toggle("color-dark");
-        elem.children[2].classList.toggle("color-white-white");
-    })
-}
-document.addEventListener("click", e => {
-    const isDropDown = e.target.matches("[data-dropdown]");
-    let dropDowns = document.querySelectorAll('[data-dropdown]');
-    let idk = e.target.querySelector(".nav__profile-dropdown");
-    if (!isDropDown && idk) {
-        for (let i = 0; i < dropDowns.length; i++) {
-            dropDowns[i].classList.remove('active');
-        }
-    }
-    e.target.classList.toggle('active');
-})
